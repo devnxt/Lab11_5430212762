9683ae8286f59dda17f34c785d180c4de8fbfe66
chanincs <c.srisongcs@gmail.com>
2014-01-29 11:44:46 +0700

    Edit by Chanin

--- Lab11_1.php
+++ Lab11_1.php
@@ -1,12 +1,6 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1265
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural
-
-\f0\fs24 \cf0 <?php \
+ <?php 
 	phpinfo();\
-	echo \'91<p>Hello World</p>\'92;\
-	echo \'91<p>What a wonderful day</p>\'92;\
-	echo \'91<p>We are having fun</p>\'92;\
-?>}
+	echo '<p>My name is Chanin</p>';
+	
+	
+?>
